0d4db4a572084b91fca2aa32c519a2bab0383442
Jake Scott <scottj@sou.edu>
2019-04-26 13:06:28 -0700

    udated scripts

--- configure-arch.sh
+++ configure-arch.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-# Sets default lanbuage to English and makes network files
+# Sets default lanbuage to English, makes network files
 # must run as root and takes 'hostname' as argument
 
 [ -z "$1" ] && echo 'No hostname supplied' && exit 1
 
 # Timezone and clock set
-echo 'Setting timezone to Los_Angeles...'
+echo "Setting timezone to Los_Angeles..."
 ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 hwclock --systohc
 date
 
 # Set language
-echo 'Setting language to en_US.UTF-8...'
+echo "Setting language to en_US.UTF-8..."
 sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
 locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 
 # Network setup
 echo "Writing network files with hostname $1..."
@@ -30,4 +30,11 @@ EOL
 cat /etc/hostname
 cat /etc/hosts
 
-echo 'Done'
+# Make pacman pretty
+grep "^Color" /etc/pacman.conf > /dev/null || sed -i "s/^#Color/Color/" /etc/pacman.conf
+
+# Disable that fucking beep
+rmmod pcspkr
+echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
+
+echo "Done"
